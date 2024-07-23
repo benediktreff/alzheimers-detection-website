d8feb4249d857d3c6638d51bf84775b2ae39b4ba
Benedikt Reff <benedikt.reff00@gmail.com>
2024-07-23 16:43:42 +0200

    Website updated (Buttons centered/text added etc.)

--- app.py
+++ app.py
@@ -53,20 +53,33 @@ if uploaded_image is not None:
         if response.status_code == 200:
             #get predicton
             response_json = response.json()
-            data_series = pd.Series([response_json['mild'], response_json['none'], response_json['very_mild']], index=["Risk of Alzheimer's Disease (stage: mild)", "Healthy", "Risk of Alzheimer's Disease (stage: very mild)"])
-            data_df = pd.DataFrame(data_series, columns=['Probability'])
-            data_df = data_df.sort_values(by='Probability', ascending=False)
-            index_of_max = data_df['Probability'].idxmax()
+            data_series = pd.Series([round(response_json['mild'],3)*100, round(response_json['none'],3)*100, round(response_json['very_mild'],3)*100], index=["Risk of Alzheimer's Disease (stage: mild)", "Healthy", "Risk of Alzheimer's Disease (stage: very mild)"])
+            data_df = pd.DataFrame(data_series, columns=['Probability (%)'])
+            data_df = data_df.sort_values(by='Probability (%)', ascending=False)
+            index_of_max = data_df['Probability (%)'].idxmax()
             #Prediction output
-            columns[2].markdown(f'<p style="font-size:18px;"><strong>{index_of_max}</strong></p>', unsafe_allow_html=True)
-            with st.expander("Click for information about probability:"):
+            #st.markdown(f'<p style="font-size:18px;text-align: center;"><strong>{index_of_max}</strong></p>', unsafe_allow_html=True)
+            st.markdown(f"""
+            <div style="border: 1px solid black; padding: 8px; background-color: #f0f0f0; border-radius: 3px;">
+                <h1 style="text-align: center; font-size: 18px;">Diagnosis: {index_of_max}</h1>
+            </div>
+            """,unsafe_allow_html=True)
+            st.markdown('')
+            st.markdown('')
+            st.markdown('')
+            with st.expander("Details about probability distribution"):
                 st.write(data_df)
             #with st.expander("Click for information about probability:"):
                 #st.write('Probability distribution')
                 #st.write(f'Healthy: {data_df.loc['Healthy', 'Probability']}')
-            with st.expander("Click for information about diagnosis"):
-                st.warning('Warning')
-                st.write('Text about diagnosis')
+            with st.expander("Details about diagnosis"):
+                st.warning("Disclaimer: This model has been developed as a tool to assist doctors in classifying the stage of Alzheimer's disease and aiding in diagnosis. It does not replace a human doctor, and any diagnosis should not be made without consultation with a qualified medical professional.")
+                if index_of_max == 'Healthy':
+                    st.write(f"With an accuracy of {round(response_json['none'],3)*100}%, it is recommended to refer the patient to a specialist in neurology or psychiatry to confirm that Alzheimer's disease can be excluded from the diagnosis.")
+                if index_of_max == "Risk of Alzheimer's Disease (stage: mild)":
+                    st.write(f"The model predicts mild dementia with an accuracy of {round(response_json['mild'],3)*100}%.. It is urged to refer the patient to a specialist in neurology or psychiatry to confirm the illness and the stage. Since the model has been trained to predict the three stages (Non-demented, Very Mild Dementia, Mild Dementia), it is important to be aware that the actual stage might also be moderate or even more advanced.")
+                if index_of_max == "Risk of Alzheimer's Disease (stage: very mild)":
+                    st.write(f"The model predicts very mild dementia with an accuracy of {round(response_json['very_mild'],3)*100}%. It is advised to consult a specialist in neurology or psychiatry to confirm the stage of dementia. While the model is trained to predict this stage, further medical evaluation is necessary to determine the precise condition.")
             #st.bar_chart(data_series)
 
         else:
