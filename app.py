3b2980c33783f9ac60d27a5d63c78829b61bb8e8
Benedikt Reff <benedikt.reff00@gmail.com>
2024-07-23 15:46:00 +0200

    Website better

--- app.py
+++ app.py
@@ -16,6 +16,14 @@ st.markdown(
     """,
     unsafe_allow_html=True)
 
+#Output style
+st.markdown("""
+<style>
+.big-font {font-size:300px !important;}
+</style>
+""",
+unsafe_allow_html=True)
+
 #Logo
 image = Image.open('Logo/Logo_round.png')
 st.logo(image, )
@@ -23,22 +31,6 @@ st.logo(image, )
 #Title
 st.markdown("<h1 style='text-align: center; color: white;'>Alzheimer's Detection Tool</h1>", unsafe_allow_html=True)
 
-#Style upload Button (does not work yet)
-st.markdown(
-    """
-    <style>
-    .stFileUpload > label {display: inline-block;
-        background-color: #4CAF50;
-        color: white;
-        padding: 10px 20px;
-        font-size: 16px;
-        border-radius: 5px;
-        cursor: pointer;}
-    .stFileUpload > label:hover {background-color: #45a049;}
-    .stFileUpload > div {display: none;}
-    </style>
-    """,
-    unsafe_allow_html=True)
 
 #Upload Button
 uploaded_image = st.file_uploader("Upload MRI scan", type='jpg',
@@ -46,33 +38,49 @@ uploaded_image = st.file_uploader("Upload MRI scan", type='jpg',
                  disabled=False,
                  label_visibility="collapsed")
 
-#devide website in two columns
-columns = st.columns(2)
+#devide website in three columns
+columns = st.columns(5)
+c = st.columns(3)
+col1, col2, col3,col4, col5 = st.columns(5)
 
 if uploaded_image is not None:
 
     # When the user clicks the 'Predict' button
-    if st.button("Get diagnosis"):
+    if columns[2].button("Get diagnosis"):
         files = {"img": uploaded_image.getvalue()}
         response = requests.post("https://alzheimers-api-g5wnkiowzq-ew.a.run.app/upload_image", files=files)
 
         if response.status_code == 200:
             #get predicton
             response_json = response.json()
-            data_series = pd.Series([response_json['mild'], response_json['none'], response_json['very_mild']], index=["Mild Alzheimer's", "No Alzheimer's", "Very mild Alzheimer's"])
+            data_series = pd.Series([response_json['mild'], response_json['none'], response_json['very_mild']], index=["Risk of Alzheimer's Disease (stage: mild)", "Healthy", "Risk of Alzheimer's Disease (stage: very mild)"])
             data_df = pd.DataFrame(data_series, columns=['Probability'])
             data_df = data_df.sort_values(by='Probability', ascending=False)
             index_of_max = data_df['Probability'].idxmax()
             #Prediction output
-            columns[1].write(f'**Diagnosis:  {index_of_max}**')
-            with st.expander("Click for detailed information"): ### does not work yet
-                columns[1].write(data_df)
-            #columns[1].bar_chart(data_series)
+            columns[2].markdown(f'<p style="font-size:18px;"><strong>{index_of_max}</strong></p>', unsafe_allow_html=True)
+            with st.expander("Click for information about probability:"):
+                st.write(data_df)
+            #with st.expander("Click for information about probability:"):
+                #st.write('Probability distribution')
+                #st.write(f'Healthy: {data_df.loc['Healthy', 'Probability']}')
+            with st.expander("Click for information about diagnosis"):
+                st.warning('Warning')
+                st.write('Text about diagnosis')
+            #st.bar_chart(data_series)
 
         else:
-            columns[1].st.write("Error:", response.text)
+            st.write("Error:", response.text)
 
     #show the uploaded image
-    image = Image.open(uploaded_image)
-    columns[0].image(image, caption='Uploaded MRI Scan.', use_column_width=True, width=10)
-    ##
+    col1, col2, col3,col4, col5 = st.columns(5)
+
+    with col1:
+        st.write(' ')
+
+    with col2:
+        image = Image.open(uploaded_image)
+        st.image(image, caption='Uploaded MRI Scan.', width=400)
+
+    with col3:
+        st.write(' ')
