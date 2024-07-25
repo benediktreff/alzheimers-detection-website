fcb7d9c3a23a2fa435bfe16ee532751a654dbc59
Benedikt Reff <benedikt.reff00@gmail.com>
2024-07-25 16:30:56 +0200

    Minor changes

--- Diagnosis.py
+++ Diagnosis.py
@@ -51,7 +51,7 @@ uploaded_image = st.file_uploader("Upload MRI scan", type='jpg',
 col1, col2, col3, col4, col5, col6, col7, col8, col9, col10, col11, col12 = st.columns(12)
 block0 = st.columns(5)
 block1 = st.columns(1)
-block2 = st.columns(5)
+
 
 
 
@@ -90,7 +90,7 @@ if uploaded_image is not None:
                 st.warning("**Disclaimer:** This model has been developed as a tool to assist doctors. Any diagnosis should not be made without consultation with a qualified medical professional.")
                 if index_of_max == 'Healthy':
                     st.write(f"With an accuracy of {str(response_json['none']*100)[:5]}%, it is recommended to refer the patient to a specialist in neurology or psychiatry to confirm that Alzheimer's disease can be excluded from the diagnosis.")
-                if index_of_max == "Mild Dementi":
+                if index_of_max == "Mild Dementia":
                     st.write(f"The model predicts mild dementia with an accuracy of {str(response_json['mild']*100)[:5]}%. It is urged to refer the patient to a specialist in neurology or psychiatry to confirm the illness and the stage. Since the model has been trained to predict the three stages (Non-demented, Very Mild Dementia, Mild Dementia), it is important to be aware that the actual stage might also be moderate or even more advanced.")
                 if index_of_max == "Very mild Dementia":
                     st.write(f"The model predicts very mild dementia with an accuracy of {str(response_json['very_mild']*100)[:5]}%. It is advised to consult a specialist in neurology or psychiatry to confirm the stage of dementia. While the model is trained to predict this stage, further medical evaluation is necessary to determine the precise condition.")
